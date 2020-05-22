fcf8d26891d8a77eef0fa4c5fee4e1209f4e06cf
Arnim Bleier <arnim.bleier@gmail.com>
2020-05-22 16:07:16 +0200

    add files

---
+++ myLib.R
@@ -0,0 +1,7 @@
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(scales))
+suppressPackageStartupMessages(library(ggthemes))
+
+options(repr.plot.width=8, repr.plot.height=3)
